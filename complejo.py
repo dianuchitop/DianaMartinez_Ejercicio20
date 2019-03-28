01a7ec30509f7b0cd71648e90135ae677a0262e3
dianuchitop <48950463+dianuchitop@users.noreply.github.com>
2019-03-28 07:48:29 -0500

    Update complejo.py

--- complejo.py
+++ complejo.py
@@ -1,19 +1,20 @@
-import unittest
+import numpy as np
 import complejo
 import math
 
-class Complejo
-   def__init__
-   self.imaginario =
-   self.real = 
-   self.norma = 
-   
-#self imaginario
-#self real
-#self norma
+class Complejo():
 
-
-#def conjugado ()
-#def calcula_norma
-#def pow()
-
+        def __init__ (self,re,im):
+            self.imaginario = im
+            self.real = re
+            self.norma = no
+        
+        def cojugado (self,re,im):
+            conjugado = (im)*(-1)
+            return conjugado
+        
+        def calcula_norma():
+            no = (re**2 + im**2)**(1/2)
+            return no
+        
+         def pow():
